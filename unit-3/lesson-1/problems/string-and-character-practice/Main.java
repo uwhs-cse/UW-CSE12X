426f96b652d17fc4cf04d4f92c1ee86da1cfbe6c
Parker <pgusto34@gmail.com>
2023-05-20 06:21:54 -0700

    Commit for the UWHS CSE 121 Teacher Training

---
+++ unit-3/lesson-1/problems/string-and-character-practice/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String message = "Hello";
+        char letter = 'l';
+        // TODO: write the code to produce the expected output below, 
+        //       printing out the number of occurrences of the 'letter' in 'message'
+    }
+}
